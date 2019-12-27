187957c918946e700b93cc6ddff882e2f7a6246e
qq1394349152 <1394349152@qq.com>
2019-12-27 17:35:09 +0800

    2019.12.27

--- routers/app.js
+++ routers/app.js
@@ -1,18 +1,15 @@
 module.exports = function(req,res,next){
-<<<<<<< Updated upstream
-    const connection = require('../lib/database')
-    connection.query('SELECT * FROM `information_help`;', function (error, results, fields) {
-        if (error) {
-            console.error(error)
-        } else {
-            console.log(results)
-            res.send({"info":results})//返回数据给前端
-        }
-=======
-    const pool = require('../lib/database');
-    c
->>>>>>> Stashed changes
-    });
+    const pool = require('../lib/database')
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;                 // 抛出异常
+        // 使用连接
+        connection.query('SELECT * FROM `information_help`;', function (error, results, fields) {
+            console.log(results)            //控制台输出数据
+            res.send({"info": results})      //返回数据给前端
+            connection.release();           //释放连接
+            if (error) throw error;         //抛出异常
+        });
+    })
 };
 
 
