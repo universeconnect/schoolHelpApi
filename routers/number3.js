7fcc789f2ceeb61c550716be72a6277ba189e5c5
kimi233 <15870290085@163.com>
2020-01-06 19:35:02 +0800

    重构sqlAPI

--- routers/number3.js
+++ routers/number3.js
@@ -15,7 +15,15 @@ module.exports = function(req,res,next){
     &&req.body.information_town
     &&req.body.information_detaiLocation
     &&req.body.information_limitSex){//判断是否接收到指定的参数
-        let sql = `INSERT INTO information_help (information_type,information_title,information_content,information_money,information_status,information_user,information_place,information_latitude,information_longitude,information_endmouthtime,information_endtime,information_phone,information_town,information_detaiLocation,information_limitSex) VALUES (${req.body.information_type},"${req.body.information_title}","${req.body.information_content}",${req.body.information_money},${req.body.information_status},${req.body.information_user},"${req.body.information_place}","${req.body.information_latitude}","${req.body.information_longitude}","${req.body.information_endmouthtime}","${req.body.information_endtime}","${req.body.information_phone}","${req.body.information_town}","${req.body.information_detaiLocation}",${req.body.information_limitSex});`;
+        let myDate = new Date();//获取当前年
+        let year = myDate.getFullYear();//获取当前月
+        let month = myDate.getMonth() + 1;//获取当前日
+        let date = myDate.getDate();
+        let h = myDate.getHours(); //获取当前小时数(0-23)
+        let m = myDate.getMinutes(); //获取当前分钟数(0-59)
+        let s = myDate.getSeconds();
+        let time = year+"-"+month+'-'+date+' '+h+":"+m+':'+s;//拼接得到当前时间
+        let sql = `INSERT INTO information_help (information_time,information_type,information_title,information_content,information_money,information_status,information_user,information_place,information_latitude,information_longitude,information_endmouthtime,information_endtime,information_phone,information_town,information_detaiLocation,information_limitSex) VALUES ("${time}",${req.body.information_type},"${req.body.information_title}","${req.body.information_content}",${req.body.information_money},${req.body.information_status},${req.body.information_user},"${req.body.information_place}","${req.body.information_latitude}","${req.body.information_longitude}","${req.body.information_endmouthtime}","${req.body.information_endtime}","${req.body.information_phone}","${req.body.information_town}","${req.body.information_detaiLocation}",${req.body.information_limitSex});`;
         sqlApi(req,sql)
         .then(function (data) {
             res.send({
